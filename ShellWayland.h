40d9407a590a89f68d93ba93ae18771dc29c46db
Mauricio Ferrari <m10ferrari1200@gmail.com>
2025-12-13 18:34:33 -0300

    Fix wayland and update wl_shell to xdg_shell

--- ShellWayland.h
+++ ShellWayland.h
@@ -18,25 +18,33 @@
 #define SHELL_WAYLAND_H
 
 #include "Shell.h"
+#include <wayland-client.h>
+#include "xdg-shell-client-protocol.h"
+#include "zxdg-decoration-client-protocol.h"
 
 class ShellWayland : public Shell {
-   public:
+public:
     explicit ShellWayland(Game &game);
+
     ~ShellWayland() override;
 
     void run() override;
+
     void quit() override { quit_ = true; }
 
-   private:
+private:
     void init_connection();
 
     PFN_vkGetInstanceProcAddr load_vk() override;
+
     bool can_present(VkPhysicalDevice phy, uint32_t queue_family) override;
 
     void create_window();
+
     VkSurfaceKHR create_surface(VkInstance instance) override;
 
     void loop_wait();
+
     void loop_poll();
 
     void *lib_handle_{};
@@ -45,38 +53,85 @@ class ShellWayland : public Shell {
     wl_display *display_{};
     wl_registry *registry_{};
     wl_compositor *compositor_{};
-    wl_shell *shell_{};
+
+    // Objects XDG Shell
+    struct xdg_wm_base *wm_base_{};
     wl_surface *surface_{};
-    wl_shell_surface *shell_surface_{};
+    struct xdg_surface *xdg_surface_{};
+    struct xdg_toplevel *xdg_toplevel_{};
+
+    // Objects XDG Decoration
+    struct zxdg_decoration_manager_v1 *decoration_manager_{};
+    struct zxdg_toplevel_decoration_v1 *toplevel_decoration_{};
+
     wl_seat *seat_{};
     wl_pointer *pointer_{};
     wl_keyboard *keyboard_{};
 
-    static void handle_ping(void *data, wl_shell_surface *shell_surface, uint32_t serial);
-    static void handle_configure(void *data, wl_shell_surface *shell_surface, uint32_t edges, int32_t width, int32_t height);
-    static void handle_popup_done(void *data, wl_shell_surface *shell_surface);
-    static const wl_shell_surface_listener shell_surface_listener;
-    static void pointer_handle_enter(void *data, struct wl_pointer *pointer, uint32_t serial, struct wl_surface *surface,
-                                     wl_fixed_t sx, wl_fixed_t sy);
-    static void pointer_handle_leave(void *data, struct wl_pointer *pointer, uint32_t serial, struct wl_surface *surface);
-    static void pointer_handle_motion(void *data, struct wl_pointer *pointer, uint32_t time, wl_fixed_t sx, wl_fixed_t sy);
-    static void pointer_handle_button(void *data, struct wl_pointer *wl_pointer, uint32_t serial, uint32_t time, uint32_t button,
-                                      uint32_t state);
-    static void pointer_handle_axis(void *data, struct wl_pointer *wl_pointer, uint32_t time, uint32_t axis, wl_fixed_t value);
+    // XDG Shell Listeners
+    static void handle_xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial);
+
+    static const struct xdg_wm_base_listener wm_base_listener;
+
+    static void handle_xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial);
+
+    static const struct xdg_surface_listener xdg_surface_listener;
+
+    static void handle_xdg_toplevel_configure(void *data, struct xdg_toplevel *xdg_toplevel,
+                                              int32_t width, int32_t height, struct wl_array *states);
+
+    static void handle_xdg_toplevel_close(void *data, struct xdg_toplevel *xdg_toplevel);
+
+    static const struct xdg_toplevel_listener xdg_toplevel_listener;
+
+    // Standard input listeners
+    static void
+    pointer_handle_enter(void *data, struct wl_pointer *pointer, uint32_t serial, struct wl_surface *surface,
+                         wl_fixed_t sx, wl_fixed_t sy);
+
+    static void
+    pointer_handle_leave(void *data, struct wl_pointer *pointer, uint32_t serial, struct wl_surface *surface);
+
+    static void
+    pointer_handle_motion(void *data, struct wl_pointer *pointer, uint32_t time, wl_fixed_t sx, wl_fixed_t sy);
+
+    static void
+    pointer_handle_button(void *data, struct wl_pointer *wl_pointer, uint32_t serial, uint32_t time, uint32_t button,
+                          uint32_t state);
+
+    static void
+    pointer_handle_axis(void *data, struct wl_pointer *wl_pointer, uint32_t time, uint32_t axis, wl_fixed_t value);
+
     static const wl_pointer_listener pointer_listener;
-    static void keyboard_handle_keymap(void *data, struct wl_keyboard *keyboard, uint32_t format, int fd, uint32_t size);
-    static void keyboard_handle_enter(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface,
-                                      struct wl_array *keys);
-    static void keyboard_handle_leave(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface);
-    static void keyboard_handle_key(void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key,
-                                    uint32_t state);
+
+    static void
+    keyboard_handle_keymap(void *data, struct wl_keyboard *keyboard, uint32_t format, int fd, uint32_t size);
+
+    static void
+    keyboard_handle_enter(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface,
+                          struct wl_array *keys);
+
+    static void
+    keyboard_handle_leave(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface);
+
+    static void
+    keyboard_handle_key(void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key,
+                        uint32_t state);
+
     static void keyboard_handle_modifiers(void *data, wl_keyboard *keyboard, uint32_t serial, uint32_t mods_depressed,
                                           uint32_t mods_latched, uint32_t mods_locked, uint32_t group);
+
     static const wl_keyboard_listener keyboard_listener;
+
     static void seat_handle_capabilities(void *data, wl_seat *seat, uint32_t caps);
+
     static const wl_seat_listener seat_listener;
-    static void registry_handle_global(void *data, wl_registry *registry, uint32_t id, const char *interface, uint32_t version);
+
+    static void
+    registry_handle_global(void *data, wl_registry *registry, uint32_t id, const char *interface, uint32_t version);
+
     static void registry_handle_global_remove(void *data, wl_registry *registry, uint32_t name);
+
     static const wl_registry_listener registry_listener;
 };
 
